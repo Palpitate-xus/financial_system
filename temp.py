c43fc1b709776ac7a8eab8e4b210ca78cdba6238
Palpitate-xus <xus1758011080@outlook.com>
2023-01-03 13:09:43 +0800

    计算无风险收益率

--- temp.py
+++ temp.py
@@ -1,5 +1,2 @@
 import akshare as ak
-import pandas
-stock_zh_index_daily_df = ak.stock_zh_index_daily(symbol="sz399552")
-for item in stock_zh_index_daily_df.values:
-    print((item[4]-item[1])/item[1])
+print(ak.bond_zh_us_rate().values[-1][2] / 100)
