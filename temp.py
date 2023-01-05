2f8dcc5a3a9db33eb432177c31af5554ea13a530
Palpitate-xus <xus1758011080@outlook.com>
2023-01-05 23:07:17 +0800

    update temp.py

--- temp.py
+++ temp.py
@@ -1,4 +1,4 @@
 import akshare as ak
 
-stock_individual_info_em_df = ak.stock_individual_info_em(symbol="000001").values[2][1]
+stock_individual_info_em_df = ak.stock_individual_info_em(symbol="399552").values[2][1]
 print(stock_individual_info_em_df.values[2][1])
